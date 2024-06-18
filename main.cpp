08c4a94e7f83503868a4ef98a7250a40315cc8a3
pietr26 <96007573+pietr26@users.noreply.github.com>
2024-06-18 16:32:40 +0200

    Reworked pdf appeareance and application functionality

--- main.cpp
+++ main.cpp
@@ -2,52 +2,56 @@
 #include <QIcon>
 #include <QPrinter>
 #include <QTextDocument>
-#include <QDateTime>
-#include <QTemporaryFile>
 #include <QDir>
+#include <QPalette>
 
-int main(int argc, char *argv[])
+void printQIcons()
 {
-    QApplication a(argc, argv);
+    qInfo() << "Preparing QIcons...";
 
-    QString html = "<html><body><table>";
+    QString html = QString("<html><body><h1>QIcons for Qt %1 on %2</h1><br/><table>").arg(qVersion(), QSysInfo().prettyProductName());
 
-    int tableIndex = 0;
-
-    QStringList errors;
-
-    int columnCount = 6;
+    int errors = 0;
+    int columnCount = 7;
 
     for (int i = 0; QIcon::ThemeIcon(i) != QIcon::ThemeIcon::NThemeIcons; i++)
     {
-
         QString filename = QDir().tempPath() + "/QIconToPdf-" + QString::number(i) + ".png";
         QImage image = QIcon::fromTheme(QIcon::ThemeIcon(i)).pixmap(32).toImage();
 
         image.invertPixels();
 
-        if (image.save(filename))
-        {
-            tableIndex++;
-            html += QString("<td><img src=\"%3\" width='32'> (%1) %2</td>").arg(QString::number(i), QIcon::fromTheme(QIcon::ThemeIcon(i)).name(), filename);
+        if (!image.save(filename)) errors++;
+        html += QString("<td style='vertical-align: middle; margin-right: 15px;'><img src='%3' width='32'>&nbsp;&nbsp;%1 | %2</td>").arg(QString::number(i), QIcon::fromTheme(QIcon::ThemeIcon(i)).name(), filename);
 
-            if (!(tableIndex % columnCount)) html += "</tr><tr>";
-        }
-        else errors << QString::number(i);
+        if (!((i + 1) % columnCount)) html += "</tr><tr>";
     }
 
-    html += QString("</tr></table></body><p><i>Can't print the follwing icons: %1</i></p></html>").arg(errors.join(", "));
+    html += QString("</tr></table></body><br/><p><i>%1 Icons failed.</i></p></html>").arg(errors);
 
+    QString filename = QCoreApplication::applicationDirPath() + QString("/QIcons_%1_%2_%3.pdf").arg(QString(qVersion()).replace(".", "_"), QSysInfo().productType(), QSysInfo().productVersion());
+
+    if (QFile(filename).exists()) QFile(filename).remove();
+
+    qInfo() << "Start printing...";
     QTextDocument document;
     document.setHtml(html);
-
     QPrinter printer(QPrinter::PrinterResolution);
     printer.setOutputFormat(QPrinter::PdfFormat);
     printer.setPageSize(QPageSize::A3);
     printer.setPageOrientation(QPageLayout::Landscape);
-    printer.setOutputFileName(QCoreApplication::applicationDirPath() + "/output_" + QDateTime().currentDateTime().toString("yyyyMMdd_hhmmss") + ".pdf");
-
+    printer.setOutputFileName(filename);
     document.print(&printer);
+
+    qInfo().noquote() << "Printing finished. Output:" << filename;
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    qApp->setPalette(QPalette(QPalette::Light));
+
+    printQIcons();
 
     return a.exec();
 }
